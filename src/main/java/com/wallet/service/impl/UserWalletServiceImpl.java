250c4c2b5c16332cc1c1e1a48ea33c14a81df8e3
Gilson <gilson.costa@dce.ufpb.br>
2020-05-03 12:31:51 -0300

    Criando o controller do UserWallet e correção no service

--- src/main/java/com/wallet/service/impl/UserWalletServiceImpl.java
+++ src/main/java/com/wallet/service/impl/UserWalletServiceImpl.java
@@ -1,11 +1,13 @@
 package com.wallet.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.wallet.entity.UserWallet;
 import com.wallet.repository.UserWalletRepository;
 import com.wallet.service.UserWalletService;
 
+@Service
 public class UserWalletServiceImpl implements UserWalletService{
 
 	@Autowired
