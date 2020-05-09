f92adfe8f43fda5b2e0f42c17dba09862261e6aa
Gilson <gilson.costa@dce.ufpb.br>
2020-05-08 21:34:00 -0300

    Teste de pesquisa por data e tipo

--- src/main/java/com/wallet/repository/WalletItemRepository.java
+++ src/main/java/com/wallet/repository/WalletItemRepository.java
@@ -1,9 +1,20 @@
 package com.wallet.repository;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wallet.entity.WalletItem;
+import com.wallet.util.enums.TypeEnum;
 
 public interface WalletItemRepository extends JpaRepository<WalletItem, Long> {
+
+	Page<WalletItem> findAllByWalletIdAndDateGreaterThanEqualAndDateLessThanEqual(Long savedWalletId, Date date,
+			Date currentDatePlusFiveDays, PageRequest pg);
+
+	List<WalletItem> findByWalletIdAndType(Long savedWalletId, TypeEnum type);
 
 }
