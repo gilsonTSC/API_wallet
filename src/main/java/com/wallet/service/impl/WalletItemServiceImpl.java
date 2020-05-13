f6505f4a80c8c6cc8e225faefa51262141bd5ccc
Gilson <gilson.costa@dce.ufpb.br>
2020-05-12 21:16:27 -0300

    Correção no controller e noservice do wallet item

--- src/main/java/com/wallet/service/impl/WalletItemServiceImpl.java
+++ src/main/java/com/wallet/service/impl/WalletItemServiceImpl.java
@@ -11,20 +11,21 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 
 import com.wallet.entity.WalletItem;
 import com.wallet.repository.WalletItemRepository;
 import com.wallet.service.WalletItemService;
 import com.wallet.util.enums.TypeEnum;
 
-
+@Service
 public class WalletItemServiceImpl implements WalletItemService{
 
 	@Autowired
 	WalletItemRepository repository;
 	
-	@Value("${pagination.items_per_page")
-	private int itemsPerPage;
+	//@Value("${pagination.items_per_page")
+	//private int itemsPerPage;
 	
 	@Override
 	@CacheEvict(value = "findByWalletAndType", allEntries = true)
@@ -36,7 +37,7 @@ public class WalletItemServiceImpl implements WalletItemService{
 	public Page<WalletItem> findBetweenDates(Long wallet, Date start, Date end, int page) {
 		
 		@SuppressWarnings("deprecation")
-		PageRequest pg = new PageRequest(page, itemsPerPage);
+		PageRequest pg = new PageRequest(page, 10);//itemsPerPage);
 		
 		return repository.findAllByWalletIdAndDateGreaterThanEqualAndDateLessThanEqual(wallet, start, end, pg);
 	}
